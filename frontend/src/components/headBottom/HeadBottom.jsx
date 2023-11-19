9596b51719f479d1913a0383fc796a6a7429a1d3
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-11-19 19:00:00 +0530

    tried making the background dark

--- frontend/src/components/headBottom/HeadBottom.jsx
+++ frontend/src/components/headBottom/HeadBottom.jsx
@@ -5,13 +5,27 @@ function HeadBottom() {
   return (
     <>
     <div className={styles.wrapper}>
-        <span className={styles.card} id='all'><h2>All</h2></span>
-        <span className={styles.card} id='food'><h2>Food</h2></span>
-        <span className={styles.card} id='health'><h2>Health & Fitness</h2></span>
-        <span className={styles.card} id='travel'><h2>Travel</h2></span>
-        <span className={styles.card} id='movies'><h2>Movies</h2></span>
-        <span className={styles.card} id='education'><h2>Education</h2></span>
-        <span className={styles.card} id='sports'><h2>Sports</h2></span>
+        <span className={styles.card} id='all'>
+          <h2>All</h2>
+        </span>
+        <span className={styles.card} id='food'>
+          <h2>Food</h2>
+        </span>
+        <span className={styles.card} id='health'>
+          <h2>Health & Fitness</h2>
+        </span>
+        <span className={styles.card} id='travel'>
+          <h2>Travel</h2>
+        </span>
+        <span className={styles.card} id='movies'>
+          <h2>Movies</h2>
+        </span>
+        <span className={styles.card} id='education'>
+          <h2>Education</h2>
+        </span>
+        <span className={styles.card} id='sports'>
+          <h2>Sports</h2>
+        </span>
     </div>
     </>
   )
