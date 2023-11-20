2309a24b53fdae1d06f16e2bd643e8ea903c7aeb
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-11-20 09:33:48 +0530

    creating the login modal

--- frontend/src/pages/homepage/Home.jsx
+++ frontend/src/pages/homepage/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/header/Header';
 import HeadBottom from '../../components/headBottom/HeadBottom';
+import Login from '../../components/login/Login';
 
 function Home() {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = ()=> {
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = ()=> {
+    setIsLoginModalOpen(false);
+  }
+
   return (
     <>
-    <Header />
+    <Header onLoginClick={openLoginModal} />
     <HeadBottom />
     <div>
         
     </div>
+    {isLoginModalOpen && <Login onClose={closeLoginModal} />}
     </>
   )
 }
