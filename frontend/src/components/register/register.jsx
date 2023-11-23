f512dba056aaa1a7ec8072f76867313a5b34431d
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-11-23 23:28:34 +0530

    styling the modals register & login

--- frontend/src/components/register/register.jsx
+++ frontend/src/components/register/register.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoEye } from "react-icons/go";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import styles from "./register.module.css";
 
 
 function Register({ onClose }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleShowPassword = ()=> {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <>
     <div className={styles.popupModal} onClick={onClose}></div>
@@ -15,7 +21,8 @@ function Register({ onClose }) {
         <label htmlFor="username">Username</label>
         <input type="text" name="username" id="username" placeholder='Enter Username here'/>
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" placeholder='Enter Password here'/> <GoEye />
+        <input type={showPassword ? "text" : "password"} name="password" id="password" placeholder='Enter Password here'/> 
+        <GoEye className={styles.eyeIcon} onClick={handleShowPassword} />
       </span>
       <button className={styles.register}>Register</button>
     </div>
